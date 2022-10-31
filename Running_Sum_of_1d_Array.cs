0946dce6eb52deaa28c1c0724b6e7f3f8e595738
Eli <pc.ghost@gmail.com>
2022-10-31 09:15:21 +0800

    first commit

---
+++ Running_Sum_of_1d_Array.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+    
+    public int[] RunningSum(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i == 0)
+            {
+                continue;
+            }
+            else
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            
+        }
+        return nums;
+    }
+    
+    public static void Main()
+    {
+        Solution first = new Solution();
+        int[] test = { 3,1,2,10,1 };
+        int[] answer = first.RunningSum(test);
+        foreach (var i in answer)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
