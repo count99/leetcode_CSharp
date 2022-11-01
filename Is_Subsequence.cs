e4186d6afecb1870df15faabf0432560d53bf066
Eli <pc.ghost@gmail.com>
2022-11-01 08:38:13 +0800

    String

---
+++ Is_Subsequence.cs
@@ -0,0 +1,38 @@
+﻿public class Solution {
+    
+    public bool IsSubsequence(string s, string t)
+    {
+        bool answer = false;
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (s.Length == 0)
+            {
+                break;
+            }
+
+            {
+                if (t[i] == s[0])
+                {
+                    s = s.Remove(0, 1);
+                }
+            }
+        }
+
+        if (s.Length == 0)
+        {
+            answer = true;
+        }
+
+        return answer;
+    }
+
+    public static void Main()
+    {
+        string s = "";
+        string t = "abc";
+        Solution test = new Solution();
+        bool ans = test.IsSubsequence(s, t);
+        Console.Write(ans);
+    }
+}
---
+++ Isomorphic_Strings.cs
@@ -0,0 +1,47 @@
+﻿public class Solution {
+    public bool IsIsomorphic(string s, string t)
+    {
+        bool answer = true;
+        string temp_word = "";
+        Dictionary<char, char> MyDic = new Dictionary<char, char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (MyDic.ContainsKey(s[i]))
+            {
+                if (t[i] != MyDic[s[i]])
+                {
+                    answer = false;
+                    break;
+                }
+                temp_word += s[i];
+            }
+            else
+            {
+                if (MyDic.ContainsValue(t[i]))
+                {
+                    answer = false;
+                    break;
+                }
+                else
+                {
+                    MyDic.Add(s[i], t[i]);
+                    temp_word += s[i];
+                }
+            }
+        }
+        if (temp_word != s)
+        {
+            answer = false;
+        }
+        return answer;
+    }
+
+    public static void Main()
+    {
+        Solution first = new Solution();
+        string s = "bbbaaaba";
+        string t = "aaabbbba";
+        bool answer = first.IsIsomorphic(s, t);
+        Console.WriteLine(answer);
+    }
+}
