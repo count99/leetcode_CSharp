9497aa6f6104a6e3872971374c1ef4cd409b0be9
Eli <pc.ghost@gmail.com>
2022-11-03 13:55:50 +0800

    day 3

---
+++ Add_Two_Numbers.cs
@@ -0,0 +1,69 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Solution
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode now_object = new ListNode(0);
+        ListNode operate_object = now_object;
+        int carry = 0;
+        int remainder = 0;
+        while (l1 != null || l2 != null)
+        {
+            int sum = 0;
+            if (l1 != null) { sum += l1.val; }
+            if (l2 != null) { sum += l2.val; }
+            if (carry > 0)
+            {
+                sum += carry;
+                carry = sum / 10;
+                remainder = sum % 10;
+            }
+            else
+            {
+                carry = sum / 10;
+                remainder = sum % 10;
+            }
+            if (l1 != null) { l1 = l1.next; }
+            if (l2 != null) { l2 = l2.next; }
+            operate_object.next = new ListNode(remainder);
+            operate_object = operate_object.next;
+        }
+        if (carry > 0)
+        {
+            operate_object.next = new ListNode(carry);
+        }
+        return now_object.next;
+    }
+
+    public static void Main()
+    {
+        ListNode l1 = new ListNode(9);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
+        l1.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next.next = new ListNode(9);
+
+        ListNode l2 = new ListNode(9);
+        l2.next = new ListNode(9);
+        l2.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
+
+        ListNode list = AddTwoNumbers(l1, l2);
+        while (list != null)
+        {
+            Console.WriteLine(list.val);
+            list = list.next;
+        }
+    }
+}
---
+++ Two_Sum.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode1
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] pick_nums = new int[2];
+            int[] pick_nums_index = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                pick_nums[0] = nums[i];
+                pick_nums_index[0] = i;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        int sum = nums[i] + nums[j];
+                        if (sum == target)
+                        {
+                            pick_nums[1] = nums[j];
+                            pick_nums_index[1] = j;
+                            break;
+                        }
+                    }
+                }
+                if (pick_nums[0] + pick_nums[1] == target)
+                {
+                    break;
+                }
+            }
+            return pick_nums_index;
+        }
+        //public static void Main()
+        //{
+            //Solution sol = new();
+        //    int[] input1 = { 2, 7, 11, 15 };
+        //    int aNumber = 9;
+        //    System.Console.Write("[{0}]", string.Join(", ", sol.TwoSum(input1, aNumber)));
+        //}
+    }
+        
+}
