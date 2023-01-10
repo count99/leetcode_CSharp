0b53c7ed37c424190ba2d17f17a8f2a79fd17934
Eli <pc.ghost@gmail.com>
2023-01-10 15:57:37 +0800

    Linked List

---
+++ Linked_List_Cycle_II.cs
@@ -0,0 +1,64 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) { 
+        this.val = val;
+        this.next = next;
+    }
+}    
+public class Solution {
+    public ListNode DetectCycle(ListNode head)
+    {
+        int found = 0;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode aws = null;
+        while (slow != null || fast != null)
+        {
+            try
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            catch
+            {
+                break;
+            }
+            if (slow == fast)
+            {
+                found = 1;
+                break;
+            }
+        }
+        if (found == 1)
+        {
+            slow = head;
+            while (fast != slow)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            aws = slow;
+        }
+
+        return aws;
+    }
+
+    public static void Main()
+    {
+        ListNode list1 = new ListNode(3);
+        list1.next = new ListNode(2);
+        // list1.next.next = list1;
+        // list1.next.next = new ListNode(0);
+        // list1.next.next.next = new ListNode(-4);
+        // list1.next.next.next.next = list1.next;
+        
+        Solution test = new Solution();
+        ListNode answer = test.DetectCycle(list1);
+        while (answer!= null) {
+            Console.Write(answer.val + " ");
+            answer = answer.next;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Middle_of_the_Linked_List.cs
@@ -0,0 +1,66 @@
+﻿using System;
+/*You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Example:
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+ * */
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    } 
+}
+
+public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        int count = 0;
+        ListNode temp = head;
+        while (temp != null)
+        {
+            count += 1;
+            temp = temp.next;
+        }
+        temp = head;
+        count = Convert.ToInt16(Math.Floor(Convert.ToDecimal(count) / 2));
+        for (int i = 0; i<count; i++)
+        {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+
+    public static void Main()
+    {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+        l1.next.next.next.next.next = new ListNode(6);
+        // l1.next.next.next.next.next.next = new ListNode(9);
+
+        // ListNode l2 = new ListNode(9);
+        // l2.next = new ListNode(9);
+        // l2.next.next = new ListNode(9);
+        // l1.next.next.next = new ListNode(9);
+
+        Solution test = new Solution();
+        ListNode list = test.MiddleNode(l1);
+        while (list != null)
+        {
+            Console.WriteLine(list.val);
+            list = list.next;
+        }
+    }
+}
